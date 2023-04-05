c02766990664e37c73df8926870b194a5bf0f986
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-04-05 05:58:23 +0300

    quick firewall check

---
+++ pages/api/post/index.ts
@@ -0,0 +1,38 @@
+import { getSession } from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  console.log('@@@@', stringify(req))
+
+  const session = await getSession({ req });
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
+
+function stringify(obj) {
+  let cache = []
+  let str = JSON.stringify(obj, function (key, value) {
+    if (typeof value === "object" && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        // Circular reference found, discard key
+        return
+      }
+      // Store value in our collection
+      cache.push(value)
+    }
+    return value
+  })
+  cache = null // reset the cache
+  return str
+}

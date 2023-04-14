8fc43053f75bb13a000567992e3af04bb1107224
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-04-14 07:29:06 +0300

    ts typeof globalThis fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,10 +5,13 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient()
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
+	let globalWithPrisma = global as typeof globalThis & {
+		prisma: PrismaClient
+	}
+    if (!globalWithPrisma.prisma) {
+		globalWithPrisma.prisma = new PrismaClient()
+	}
+	prisma = globalWithPrisma.prisma
 }
 
 export default prisma

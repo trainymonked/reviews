d73209d8fa1149b93e7e5c01f7347821e2a9862d
trainymonked <47102288+trainymonked@users.noreply.github.com>
2023-04-18 05:05:17 +0300

    commit to refresh vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,13 +5,13 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient()
 } else {
-	let globalWithPrisma = global as typeof globalThis & {
-		prisma: PrismaClient
-	}
+    let globalWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient
+    }
     if (!globalWithPrisma.prisma) {
-		globalWithPrisma.prisma = new PrismaClient()
-	}
-	prisma = globalWithPrisma.prisma
+        globalWithPrisma.prisma = new PrismaClient()
+    }
+    prisma = globalWithPrisma.prisma
 }
 
 export default prisma
